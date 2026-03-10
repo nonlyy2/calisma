82427d0a869681911417e6e112735e47137634ce
Assylkhan <assylkhan.kerey@nu.edu.kz>
2026-03-10 14:06:30 +0500

    refac logic: 19. Remove Nth Node From End of List(leetcode)

--- leetcode/19_remove_nth_node_from_end_of_list.go
+++ leetcode/19_remove_nth_node_from_end_of_list.go
@@ -7,11 +7,11 @@ func RemoveNthFromEnd(head *ListNode, n int) *ListNode {
 	dummy := &ListNode{Next: head}
 	slow, fast := dummy, dummy
 
-	for i := 0; i <= n; i++ {
+	for i := 0; i < n; i++ {
 		fast = fast.Next
 	}
 
-	for fast != nil {
+	for fast.Next != nil {
 		slow = slow.Next
 		fast = fast.Next
 	}
